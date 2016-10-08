e6621ef546ecd5749f3fa1936737a751fab64f40
Andres Barradas <arbadev@gmail.com>
2016-10-07 20:45:38 -0400

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ncrDuplicateFrontApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ncrDuplicateFrontApp
+ */
+angular.module('ncrDuplicateFrontApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ncrDuplicateFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ncrDuplicateFrontApp
+ */
+angular.module('ncrDuplicateFrontApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
