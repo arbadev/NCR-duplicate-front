aeacbd1a228caa75f753d546dc6ac62a48575931
Andres Barradas <arbadev@gmail.com>
2016-10-08 21:14:21 -0400

    added Duplicate POST request

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,35 @@
 'use strict';
 
 /**
- * @ngdoc function
- * @name ncrDuplicateFrontApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the ncrDuplicateFrontApp
- */
+* @ngdoc function
+* @name ncrDuplicateFrontApp.controller:MainCtrl
+* @description
+* # MainCtrl
+* Controller of the ncrDuplicateFrontApp
+*/
 angular.module('ncrDuplicateFrontApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+.controller('MainCtrl', function ($scope, $http) {
+  $scope.SendData = function () {
+    console.log('duplicate', $scope.duplicate);
+    // use $.param jQuery function to serialize data from JSON
+    var data = {
+      duplicate: $scope.duplicate
+    };
+
+    var config = {
+      headers : {
+        'Content-Type': 'application/json'
+      }
+    }
+    $http.post('http://localhost:8443/duplicates', data, config)
+    .success(function (data, status, headers, config) {
+      $scope.PostDataResponse = data;
+    })
+    .error(function (data, status, header, config) {
+      $scope.ResponseDetails = "Data: " + data +
+      "<hr />status: " + status +
+      "<hr />headers: " + header +
+      "<hr />config: " + config;
+    });
+  };
+});
